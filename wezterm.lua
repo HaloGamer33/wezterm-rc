40f34c7c6c470f4aa27fdf05f1c8712f6b2e61c7
HaloGamer33 <106188903+HaloGamer33@users.noreply.github.com>
2025-09-30 17:14:35 -0600

    feat(tab_titles): change tab titles to be tab_index

--- wezterm.lua
+++ wezterm.lua
@@ -9,6 +9,29 @@ end
 -- │                        Asthetics                        │
 -- └─────────────────────────────────────────────────────────┘
 
+-- This function returns the suggested title for a tab.
+-- It prefers the title that was set via `tab:set_title()`
+-- or `wezterm cli set-tab-title`, but falls back to the
+-- title of the active pane in that tab.
+function tab_title(tab_info)
+  local title = tab_info.tab_title
+  -- if the tab title is explicitly set, take that
+  if title and #title > 0 then
+    return title
+  end
+  -- Otherwise, use the title from the active pane
+  -- in that tab
+  return string.format(' %i ', tab_info.tab_index + 1)
+end
+
+wezterm.on(
+    'format-tab-title',
+    function(tab, tabs, panes, config, hover, max_width)
+        local title = tab_title(tab)
+        return title
+    end
+)
+
 config.color_scheme = 'Oxocarbon Dark'
 config.use_fancy_tab_bar = false
 config.font_size = 14
